11a8c9d541ec699d8d41ad90fcdf8ccc6dda44e5
Vladimir Reymer <vladimireymer@gmail.com>
2024-06-28 10:28:57 +0700

    Comments

--- server/proxy.py
+++ server/proxy.py
@@ -1,13 +1,16 @@
-import socket
-from sys import argv
 from configparser import ConfigParser
 from select import select
+import socket
 
 from wsgi_client import Client
 
 
 class Server:
+    """Proxy server for redirect requests to the WSGI (application) server"""
+
     def __init__(self, host: str, port: int) -> None:
+        """Set proxy host and port, create socket"""
+
         self.__host = host
         self.__port = port
 
@@ -17,6 +20,7 @@ class Server:
         self.__sock.bind((self.__host, self.__port))
         self.__sock.listen()
 
+        # Add client acception method to the task for event loop
         tasks.append(self.accept_connection())
 
         print(f"Server started on http://{self.__host}:{self.__port}")
@@ -25,21 +29,26 @@ class Server:
         """Accept connections and start request handling for clients"""
 
         while True:
-            yield ('read', self.__sock)
+            yield ('read', self.__sock)  # Return socket for event loop and stop here
+
+            # Accept client and add handling method to the task for event loop
             client_sock, client_addr = self.__sock.accept()
             tasks.append(self.handle_connection(client_sock))
 
     def handle_connection(self, client_socket: socket.socket):
-        yield ('read', client_socket)
-        request = client_socket.recv(1024)
-        print(request)
+        """Redirect request to the application server and send back ready data to the client"""
+
+        yield ('read', client_socket)  # Return socket for event loop and stop here
+        request = client_socket.recv(1024)  # Get data from client
 
         if request:
-            wsgi_client.create_connection()
-            wsgi_client.send(request)
-            headers, body = wsgi_client.recieve()
+            wsgi_client.create_connection()  # Create connection with application server
+            wsgi_client.send(request)  # Redirect request to the WSGI (application) server
+            headers, body = wsgi_client.recieve()  # Recieve data from application server
 
-            yield ('write', client_socket)
+            yield ('write', client_socket)  # Return socket for event loop and stop here
+
+            # Send data back to the client
             client_socket.sendall(headers)
             client_socket.sendall(body)
 
@@ -58,12 +67,15 @@ def load_config(config_file: str) -> tuple[str, int, str, int]:
 
 
 def event_loop():
-    rdict, wdict = dict(), dict()
+    """Event loop to populate tasks and execute generators code"""
+
+    rdict, wdict = dict(), dict()  # Dicts for sock:generators pairs
 
     while any([tasks, rdict, wdict]):
         while not tasks:
-            ready_to_read, ready_to_write, _ = select(rdict, wdict, [])
+            ready_to_read, ready_to_write, _ = select(rdict, wdict, [])  # Get sock ready for action
 
+            # Fill tasks list for executing
             for sock in ready_to_read:
                 tasks.append(rdict.pop(sock))
             for sock in ready_to_write:
@@ -71,8 +83,9 @@ def event_loop():
 
         try:
             task = tasks.pop(0)  # Get first generator
-            reason, sock = next(task)  # Iterate to the next yield
+            reason, sock = next(task)  # Iterate to the next yield (execute code)
 
+            # Fill dicts for select()
             match reason:
                 case 'read': rdict[sock] = task
                 case 'write': wdict[sock] = task
@@ -82,9 +95,8 @@ def event_loop():
 
 
 if __name__ == '__main__':
-    tasks = list()
-
     host, port, wsgi_host, wsgi_port = load_config('config.ini')
+    tasks = list()  # Generators for event loop
     proxy = Server(host, port)
     wsgi_client = Client(wsgi_host, wsgi_port)
 
--- server/wsgi_client.py
+++ server/wsgi_client.py
@@ -2,7 +2,11 @@ import socket
 
 
 class Client:
+    """Client for redirect requests from proxy server to the application server"""
+
     def __init__(self, wsgi_host: str, wsgi_port: int) -> None:
+        """Set host and port for application server"""
+
         self.__wsgi_host = wsgi_host
         self.__wsgi_port = wsgi_port
 
@@ -16,9 +20,13 @@ class Client:
         self.__client_sock.connect(self.__wsgi)
 
     def send(self, data: bytes) -> None:
+        """Send request to the application server"""
+
         self.__client_sock.sendall(data)
 
     def recieve(self) -> tuple[bytes, bytes]:
+        """Recieve data from application server"""
+
         headers = self.__client_sock.recv(1024)
         body = self.__client_sock.recv(1024)
         self.__client_sock.close()
